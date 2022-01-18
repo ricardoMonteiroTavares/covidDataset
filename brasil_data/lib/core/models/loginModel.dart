30217d2d703a5f56084f9ba216c91f5b5b49f9ca
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-18 12:39:07 -0300

    #003 - Adição do Model para o login

---
+++ brasil_data/lib/core/models/loginModel.dart
@@ -0,0 +1,7 @@
+/// Modelo que representa os parâmetros para o login do usuário
+class LoginModel {
+  late String email;
+  late String password;
+
+  LoginModel({this.email = '', this.password = ''});
+}
