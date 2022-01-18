ebd272421f38eb8ec6e2d27be88c04eac2a52acb
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-18 12:50:03 -0300

    #003 - Criação do repositório do usuário

---
+++ brasil_data/lib/core/repositories/impl/userRepository.dart
@@ -0,0 +1,15 @@
+import 'package:brasil_data/core/models/loginModel.dart';
+import 'package:brasil_data/core/repositories/interface/repository.dart';
+
+/// Repositório que busca os dados dos usuários
+class UserRepository implements Repository<LoginModel> {
+  @override
+  Future get(LoginModel data) async {
+    if (data.email == "admin@brasildata.com" && data.password == "admin1234") {
+      await Future.delayed(const Duration(seconds: 2));
+      return {"user": "Administrador", "e-mail": data.email};
+    } else {
+      throw Exception("Error: E-mail e/ou senha inválidos");
+    }
+  }
+}
