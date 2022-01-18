8af4fea1095072c365d25ea232342cb8084ad7d5
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-18 11:20:35 -0300

    #001 - Ajuste no tipo de retorno na interface de repositório

--- brasil_data/lib/core/repositories/interface/repository.dart
+++ brasil_data/lib/core/repositories/interface/repository.dart
@@ -2,5 +2,5 @@
 /// cuja a entrada de dados deverá ser do tipo [T]
 abstract class Repository<T> {
   /// A função [get] faz a busca em um banco de dados remoto com os parâmetros requisitados em [data]
-  dynamic get(T data);
+  Future<dynamic> get(T data);
 }
