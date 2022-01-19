7b7f3c54b6b6148adec443229bcf6d5d54174f91
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-18 23:47:47 -0300

    #004 - Criação da interface de serviço

---
+++ brasil_data/lib/core/services/interface/service.dart
@@ -0,0 +1,9 @@
+import 'package:brasil_data/core/models/model.dart';
+import 'package:brasil_data/core/util/or.dart';
+
+/// Interface para a realiação de um serviço.
+/// A classe [T] é o model que representa a entrada de dados
+/// A classe [K] é o model que representa a saída de dados
+abstract class Service<T extends InputModel, K extends OutputModel> {
+  Future<Or<K, String>> action(T data);
+}
