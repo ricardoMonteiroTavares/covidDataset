ea70e118bc268d88b38b345f678142532b8c9666
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-21 10:07:08 -0300

    #025 - Criação do serviço para a busca de dados local

--- brasil_data/lib/core/repositories/impl/local_repository.dart
+++ brasil_data/lib/core/repositories/impl/local_repository.dart
@@ -1,3 +1,4 @@
+import 'package:brasil_data/core/exceptions/internet_exceptions.dart';
 import 'package:brasil_data/core/models/user_model.dart';
 import 'package:brasil_data/core/repositories/interface/repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,23 +6,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Classe que realiza a comunicação com o banco de dados local
 class LocalRepository implements Repository<UserModel> {
   @override
-  Future get([UserModel? data]) async {
+  Future<UserModel> get([UserModel? data]) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? name, email, dateTime;
     if ((name = prefs.getString("name")) == null) {
-      return null;
+      throw NotFoundException();
     }
     if ((email = prefs.getString("email")) == null) {
-      return null;
+      throw NotFoundException();
     }
     if ((dateTime = prefs.getString("dateTime")) == null) {
-      return null;
+      throw NotFoundException();
     }
 
     DateTime now = DateTime.now();
     DateTime actual = DateTime.parse(dateTime!);
     if (now.difference(actual).inHours >= 3) {
-      return null;
+      throw NotFoundException();
     }
     prefs.setString("dateTime", now.toString());
     return UserModel(email: email, name: name);
