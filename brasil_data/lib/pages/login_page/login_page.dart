35d4f606a28980983cc2f816ac810c77375a040c
Ricardo Tavares <ricardotavares51@hotmail.com>
2022-01-21 12:29:53 -0300

    #026 - Correção de bug do focus node

--- brasil_data/lib/pages/login_page/login_page.dart
+++ brasil_data/lib/pages/login_page/login_page.dart
@@ -17,96 +17,108 @@ class _LoginPageState extends State<LoginPage> {
   final _space = const SizedBox(
     height: 15,
   );
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
   @override
   void dispose() {
     _bloc.closeStream();
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    _submitFocus.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return RawKeyboardListener(
-      autofocus: true,
-      focusNode: FocusNode(),
-      onKey: (event) {
-        if (event.isKeyPressed(LogicalKeyboardKey.enter)) {
-          _bloc.signInHandler(context);
-        }
-      },
-      child: StreamBuilder(
-        stream: _bloc.stream,
-        builder: (context, snapshot) => Scaffold(
-          body: Container(
-            padding: const EdgeInsets.all(20),
-            child: Center(
-              child: Form(
-                key: _bloc.formKey,
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/icons/logo_colored.png",
-                        width: 300,
-                      ),
-                      AlertErrorWidget(
-                        msg: _bloc.errorMsg,
-                        maxWidth: _width,
-                        minHeight: _height,
-                      ),
-                      _space,
-                      Container(
-                        constraints: BoxConstraints(maxWidth: _width),
-                        child: TextFormField(
-                          focusNode: FocusNode(),
-                          decoration: const InputDecoration(
-                            icon: Icon(Icons.alternate_email),
-                            labelText: "E-mail",
-                          ),
-                          onChanged: _bloc.setEmail,
-                          validator: FormValidator.validateEmail,
+    return StreamBuilder(
+      stream: _bloc.stream,
+      builder: (context, snapshot) => Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Form(
+              key: _bloc.formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/icons/logo_colored.png",
+                      width: 300,
+                    ),
+                    AlertErrorWidget(
+                      msg: _bloc.errorMsg,
+                      maxWidth: _width,
+                      minHeight: _height,
+                    ),
+                    _space,
+                    Container(
+                      constraints: BoxConstraints(maxWidth: _width),
+                      child: TextFormField(
+                        autofocus: true,
+                        focusNode: _emailFocus,
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.alternate_email),
+                          labelText: "E-mail",
                         ),
+                        onChanged: _bloc.setEmail,
+                        validator: FormValidator.validateEmail,
+                        textInputAction: TextInputAction.next,
+                        onFieldSubmitted: (value) {
+                          _emailFocus.unfocus();
+                          FocusScope.of(context).requestFocus(_passwordFocus);
+                        },
                       ),
-                      _space,
-                      Container(
-                        constraints: BoxConstraints(maxWidth: _width),
-                        child: TextFormField(
-                          focusNode: FocusNode(),
-                          decoration: InputDecoration(
-                            icon: const Icon(Icons.password),
-                            labelText: "Senha",
-                            suffixIcon: IconButton(
-                              icon: _bloc.icon,
-                              onPressed: _bloc.toggleHandler,
+                    ),
+                    _space,
+                    Container(
+                      constraints: BoxConstraints(maxWidth: _width),
+                      child: TextFormField(
+                        autofocus: true,
+                        focusNode: _passwordFocus,
+                        decoration: InputDecoration(
+                          icon: const Icon(Icons.password),
+                          labelText: "Senha",
+                          suffixIcon: IconButton(
+                            icon: _bloc.icon,
+                            onPressed: _bloc.toggleHandler,
+                          ),
+                        ),
+                        obscureText: _bloc.obscureText,
+                        onChanged: _bloc.setPassword,
+                        validator: FormValidator.validatePassword,
+                        textInputAction: TextInputAction.done,
+                        onFieldSubmitted: (value) {
+                          _passwordFocus.unfocus();
+                          FocusScope.of(context).requestFocus(_submitFocus);
+                        },
+                      ),
+                    ),
+                    _space,
+                    ElevatedButton(
+                      focusNode: _submitFocus,
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(_width, _height),
+                      ),
+                      child: (_bloc.isLoading)
+                          ? const SizedBox(
+                              width: 20,
+                              height: 20,
+                              child: CircularProgressIndicator(
+                                color: Colors.white,
+                                strokeWidth: 1,
+                              ),
+                            )
+                          : const Text(
+                              "Entrar",
+                              style: TextStyle(color: Colors.white),
                             ),
-                          ),
-                          obscureText: _bloc.obscureText,
-                          onChanged: _bloc.setPassword,
-                          validator: FormValidator.validatePassword,
-                        ),
-                      ),
-                      _space,
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(_width, _height),
-                        ),
-                        child: (_bloc.isLoading)
-                            ? const SizedBox(
-                                width: 20,
-                                height: 20,
-                                child: CircularProgressIndicator(
-                                  color: Colors.white,
-                                  strokeWidth: 1,
-                                ),
-                              )
-                            : const Text("Entrar"),
-                        onPressed: (_bloc.isLoading)
-                            ? null
-                            : () => _bloc.signInHandler(context),
-                      )
-                    ],
-                  ),
+                      onPressed: (_bloc.isLoading)
+                          ? null
+                          : () => _bloc.signInHandler(context),
+                    )
+                  ],
                 ),
               ),
             ),
